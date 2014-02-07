4e01ea9d99b391368d3c5d129baa31f47591f793
shaikatcse@gmail.com <shaikatcse@gmail.com>
2014-02-07 21:36:50 +0100

    Bug fixed in RepairSolution class

--- src/reet/fbk/eu/OptimizeEnergyPLANWithAccuracy/util/RepairSolution.java
+++ src/reet/fbk/eu/OptimizeEnergyPLANWithAccuracy/util/RepairSolution.java
@@ -1,7 +1,22 @@
 package reet.fbk.eu.OptimizeEnergyPLANWithAccuracy.util;
 
+import java.util.HashMap;
+
+
+
+
+
+
+
+import jmetal.core.Problem;
 import jmetal.core.Solution;
 import jmetal.core.Variable;
+import jmetal.encodings.variable.Real;
+import jmetal.problems.singleObjective.Sphere;
+import jmetal.util.JMException;
+
+
+
 
 public class RepairSolution {
 
@@ -33,10 +48,54 @@ public class RepairSolution {
 				solution.getDecisionVariables()[5],
 				solution.getDecisionVariables()[6]);
 
-		for (int j = 0; j < 3; j++) {
-			solution.getDecisionVariables()[j] = fuelShare[j];
+		for (int j = 4; j < 7; j++) {
+			solution.getDecisionVariables()[j] = fuelShare[j-4];
 		}
 		
 		return solution;
 	}
+
+public static void main(String args[]) throws JMException{
+	
+		// TODO Auto-generated method stub
+		HashMap hm = new HashMap();
+
+		double distributionIndex = 20.0;
+
+		hm.put("probability", 1.0);
+		hm.put("distributionIndex", distributionIndex);
+
+		Problem problem = new Sphere("Real", 7);
+
+		Real p1[] = new Real[7];
+		p1[0] = new Real();
+		p1[1] = new Real();
+		p1[2] = new Real();
+		p1[3] = new Real();
+		p1[4] = new Real();
+		p1[5] = new Real();
+		p1[6] = new Real();
+
+		p1[0].setValue(1565.12);
+		p1[1].setValue(2568.12);
+		p1[2].setValue(5648.12);
+		p1[3].setValue(2354.12);
+		p1[4].setValue(0.124);
+		p1[5].setValue(0.874);
+		p1[6].setValue(0.446);
+		//p1[0].setValue(b);
+		
+		
+		Real p2[] = new Real[1];
+		p2[0] = new Real();
+
+		Solution s1 = new Solution();
+		s1.setDecisionVariables((Variable[]) p1);
+		
+		RepairSolution rs=new RepairSolution();
+		rs.doRepair(s1);
+		
+		
+}
+
 }
