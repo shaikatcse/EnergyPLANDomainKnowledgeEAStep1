99ed9910134080488c6db5b496272fd51461d330
shaikatcse@gmail.com <shaikatcse@gmail.com>
2014-02-08 15:42:08 +0100

    Main method with seed
    Now, The main method can deal with seed to generate same initial
    population for each test.

--- src/reet/fbk/eu/OptimizeEnergyPLANWithAccuracy/problem/EnergyPLANProblemStep1Main.java
+++ src/reet/fbk/eu/OptimizeEnergyPLANWithAccuracy/problem/EnergyPLANProblemStep1Main.java
@@ -14,8 +14,11 @@ import jmetal.operators.selection.SelectionFactory;
 import jmetal.qualityIndicator.QualityIndicator;
 import jmetal.util.Configuration;
 import jmetal.util.JMException;
-
+import jmetal.util.PseudoRandom;
+import jmetal.util.RandomGenerator;
 import reet.fbk.eu.OprimizeEnergyPLAN.jmetal.operators.mutation.MutationFactory;
+//import jmetal.operators.mutation.MutationFactory;
+
 
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
@@ -41,45 +44,56 @@ public class EnergyPLANProblemStep1Main {
 		HashMap parameters; // Operator parameters
 
 		QualityIndicator indicators; // Object to get quality indicators
-
-		indicators = null;
-
-		problem = new EnergyPLANProblemStep1("Real");
-		algorithm = new NSGAIIForDK(problem);
-		// algorithm = new ssNSGAII(problem);
-
-		// Algorithm parameters
-		algorithm.setInputParameter("populationSize", 4);
-		algorithm.setInputParameter("maxEvaluations", 20);
-
-		// Mutation and Crossover for Real codification
-		parameters = new HashMap();
-		parameters.put("probability", 0.9);
-		parameters.put("distributionIndex", 4.0);
-		crossover = CrossoverFactory.getCrossoverOperator("SBXCrossover",
-				parameters);
-
-		parameters = new HashMap();
-		parameters.put("probability", 0.2);
-		parameters.put("distributionIndex", 4.0);
-		mutation = MutationFactory.getMutationOperator("GeneralRealMutationForRes",
-				parameters);
-
-		// Selection Operator
-		parameters = null;
-		selection = SelectionFactory.getSelectionOperator("BinaryTournament2",
-				parameters);
-
-		// Add the operators to the algorithm
-		algorithm.addOperator("crossover", crossover);
-		algorithm.addOperator("mutation", mutation);
-		algorithm.addOperator("selection", selection);
-
-		// Add the indicator object to the algorithm
-		algorithm.setInputParameter("indicators", indicators);
 		
-		int numberOfRun=1;
+		long seed [] = {2121979, 13031984 };
+		
+		int numberOfRun=2;
 		for (int i = 0; i < numberOfRun; i++) {
+			
+			
+			PseudoRandom.setRandomGenerator(new RandomGenerator(seed[i]));
+
+			
+			indicators = null;
+
+			problem = new EnergyPLANProblemStep1("Real");
+			algorithm = new NSGAIIForDK(problem);
+			// algorithm = new ssNSGAII(problem);
+
+			// Algorithm parameters
+			algorithm.setInputParameter("populationSize", 4);
+			algorithm.setInputParameter("maxEvaluations", 12);
+
+			// Mutation and Crossover for Real codification
+			parameters = new HashMap();
+			parameters.put("probability", 0.9);
+			parameters.put("distributionIndex", 4.0);
+			crossover = CrossoverFactory.getCrossoverOperator("SBXCrossover",
+					parameters);
+
+			parameters = new HashMap();
+			parameters.put("probability", 0.2);
+			parameters.put("distributionIndex", 4.0);
+			/*mutation = MutationFactory.getMutationOperator("PolynomialMutation",
+						parameters);*/
+
+			mutation = MutationFactory.getMutationOperator("GeneralRealMutationForRes",
+					parameters);
+
+			// Selection Operator
+			parameters = null;
+			selection = SelectionFactory.getSelectionOperator("BinaryTournament2",
+					parameters);
+
+			// Add the operators to the algorithm
+			algorithm.addOperator("crossover", crossover);
+			algorithm.addOperator("mutation", mutation);
+			algorithm.addOperator("selection", selection);
+
+			// Add the indicator object to the algorithm
+			algorithm.setInputParameter("indicators", indicators);
+			
+			
 			// Execute the Algorithm
 			long initTime = System.currentTimeMillis();
 			SolutionSet population = algorithm.execute();
