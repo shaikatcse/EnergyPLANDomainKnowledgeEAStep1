f2db6f22991f9d2d777e4708f10221b6e5b4f886
shaikatcse <shaikatcse@gmail.com>
2014-06-17 17:06:56 +0200

    Two utility file is add
    - can take VAR file and simulate every solutions to extract required
    parameters (e.g.: production of different units)
    - can take VAR file and create two files with feasible solutions (no
    constraints violation)

---
+++ src/reet/fbk/eu/util/ExtractFeasibleSolution.java
@@ -0,0 +1,81 @@
+package reet.fbk.eu.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.util.StringTokenizer;
+
+import reet.fbk.eu.OptimizeEnergyPLANAalborg.problem.EnergyPLANProblemAalborg;
+import reet.fbk.eu.OptimizeEnergyPLANAalborg.problem.EnergyPLANProblemAalborg2Objectives;
+import jmetal.core.Algorithm;
+import jmetal.core.Operator;
+import jmetal.core.Problem;
+import jmetal.core.Solution;
+import jmetal.core.SolutionSet;
+import jmetal.util.JMException;
+
+public class ExtractFeasibleSolution {
+
+	public ExtractFeasibleSolution() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) throws JMException,
+			SecurityException, IOException, ClassNotFoundException {
+
+		Problem problem; // The problem to solve
+		Algorithm algorithm; // The algorithm to use
+		Operator crossover; // Crossover operator
+		Operator mutation; // Mutation operator
+		Operator selection; // Selection operator
+
+		SolutionSet population;
+
+		FileInputStream fos = new FileInputStream(args[0]);
+		InputStreamReader isr = new InputStreamReader(fos);
+		BufferedReader br = new BufferedReader(isr);
+
+		LineNumberReader lnr = new LineNumberReader(new FileReader(new File(
+				args[0])));
+		lnr.skip(Long.MAX_VALUE);
+		int totalNumberOfIndividuals = lnr.getLineNumber();
+		lnr.close();
+
+		String line;
+
+		population = new SolutionSet(totalNumberOfIndividuals);
+
+		problem = new EnergyPLANProblemAalborg2Objectives("Real");
+
+		Solution newSolution;
+		for (int i = 0; i < totalNumberOfIndividuals; i++) {
+			line = br.readLine();
+			StringTokenizer st = new StringTokenizer(line);
+			newSolution = new Solution(problem);
+			int j = 0;
+			while (st.hasMoreTokens()) {
+				newSolution.getDecisionVariables()[j].setValue(Double
+						.parseDouble(st.nextToken()));
+				j++;
+			}
+
+			problem.evaluate(newSolution);
+			problem.evaluateConstraints(newSolution);
+			population.add(newSolution);
+		}
+
+		File filetmp = new File(args[0]);
+		String arrayStr[]=args[0].split("\\\\");
+		
+		//File file = new File(filetmp.getParent() + "\\allParameters_"+arrayStr[arrayStr.length-1] +".txt");
+		population.printFeasibleFUN(filetmp.getParent() + "\\FUN0" +"Feasible");
+		population.printFeasibleVAR(filetmp.getParent() + "\\"+arrayStr[arrayStr.length-1] +"Feasible");
+		
+		br.close();
+	}
+
+}
