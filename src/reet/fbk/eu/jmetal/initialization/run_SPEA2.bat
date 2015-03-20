a06d91b07ae5846c4c7f34a22e1d8d60d91e291a
shaikatcse <shaikatcse@gmail.com>
2015-03-20 10:51:45 +0100

    run example for SPEA2 and SPEA2_SI, where the whole project is exported as a jar file

---
+++ src/reet/fbk/eu/jmetal/initialization/run_SPEA2.bat
@@ -0,0 +1,3 @@
+cd\
+cd Users\mahbub\Desktop\test_jar
+java -Djava.ext.dirs=C:\Users\mahbub\Desktop\test_jar -cp c:\Users\mahbub\Desktop\test_jar\test.jar reet.fbk.eu.jmetal.initialization.AalborgProblemWithTrackingMain "SPEA2"
