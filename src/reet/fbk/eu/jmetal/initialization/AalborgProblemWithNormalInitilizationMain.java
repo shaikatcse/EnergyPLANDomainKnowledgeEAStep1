dd15a81619f54aa4e79ef6737eaef67ec89a61df
shaikatcse <shaikatcse@gmail.com>
2015-04-21 12:30:13 +0200

    The main file can run from argument

--- src/reet/fbk/eu/jmetal/initialization/AalborgProblemWithNormalInitilizationMain.java
+++ src/reet/fbk/eu/jmetal/initialization/AalborgProblemWithNormalInitilizationMain.java
@@ -8,9 +8,6 @@ import jmetal.core.Algorithm;
 import jmetal.core.Operator;
 import jmetal.core.Problem;
 import jmetal.core.SolutionSet;
-import reet.fbk.eu.jmetal.metaheuristics.nsgaII.NSGAIIForDK;
-import reet.fbk.eu.jmetal.metaheuristics.nsgaII.NSGAIIForSI;
-import reet.fbk.eu.jmetal.metaheuristics.nsgaII.NSGAIITrackIndicators;
 import jmetal.metaheuristics.nsgaII.NSGAII;
 import jmetal.metaheuristics.spea2.SPEA2;
 import jmetal.operators.crossover.CrossoverFactory;
@@ -23,45 +20,33 @@ import jmetal.util.RandomGenerator;
 //import reet.fbk.eu.jmetal.operators.mutation.MutationFactory;
 import jmetal.operators.mutation.MutationFactory;
 
-
-
-
-
-
-
-
-
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
-public class AalborgProblemWithNormalInitilizationMain {
 
+class NSGAII_Run{
 	public static Logger logger_; // Logger object
 	public static FileHandler fileHandler_; // FileHandler object
 
-	public static void main(String[] args) throws JMException,
-			SecurityException, IOException, ClassNotFoundException {
+	
+	Problem problem; // The problem to solve
+	Algorithm algorithm; // The algorithm to use
+	Operator crossover; // Crossover operator
+	Operator mutation; // Mutation operator
+	Operator selection; // Selection operator
 
+	HashMap parameters; // Operator parameters
+
+	QualityIndicator indicators; // Object to get quality indicators
+	
+	public void run() throws SecurityException, IOException, ClassNotFoundException, JMException{
 		logger_ = Configuration.logger_;
 		fileHandler_ = new FileHandler("NSGAII_main.log");
 		logger_.addHandler(fileHandler_);
 
-		Problem problem; // The problem to solve
-		Algorithm algorithm; // The algorithm to use
-		Operator crossover; // Crossover operator
-		Operator mutation; // Mutation operator
-		Operator selection; // Selection operator
-
-		HashMap parameters; // Operator parameters
-
-		QualityIndicator indicators; // Object to get quality indicators
-		
 		//seed for nsgaii
-		//long seed [] = {545782, 455875, 547945, 458478, 981354, 652262, 562366, 365652, 456545, 549235 };
+		long seed [] = {545782, 455875, 547945, 458478, 981354, 652262, 562366, 365652, 456545, 549235 };
 		
-		//seed for spea2
-
-		long seed [] = {154568, 148456, 447514, 458475, 274587, 712584, 975572, 585464, 467542, 686544 };
 		int numberOfRun=10;
 		for (int i = 0; i < numberOfRun; i++) {
 			
@@ -73,6 +58,101 @@ public class AalborgProblemWithNormalInitilizationMain {
 			problem = new EnergyPLANProblemAalborg2ObjectivesWith1EnergyPLANEvolution("Real");
 			//algorithm = new NSGAIITrackIndicators(problem, seed[i], "SBX_DKMutation");
 			
+			parameters = new HashMap();
+						
+			algorithm = new NSGAII(problem);
+			// algorithm = new ssNSGAII(problem);
+
+			//indicators = new QualityIndicator(problem, "C:\\Users\\Nusrat\\Documents\\GitHub\\EnergyPLANDomainKnowledgeEAStep1\\Results\\truePf\\mergefun.pf") ;
+			
+			
+			// Algorithm parameters
+			algorithm.setInputParameter("populationSize", 100);
+			algorithm.setInputParameter("maxEvaluations", 5000);
+			
+			
+			// Mutation and Crossover for Real codification
+			parameters = new HashMap();
+			parameters.put("probability", 0.9);
+			parameters.put("distributionIndex", 10.0);
+			crossover = CrossoverFactory.getCrossoverOperator("SBXCrossover",
+					parameters);
+
+			parameters = new HashMap();
+			parameters.put("probability", 0.1);
+			parameters.put("distributionIndex", 10.0);
+			mutation = MutationFactory.getMutationOperator("PolynomialMutation",
+						parameters);
+
+			//mutation = MutationFactory.getMutationOperator("GeneralRealMutationForRes",
+				//.	parameters);
+
+			// Selection Operator
+			parameters = null;
+			selection = SelectionFactory.getSelectionOperator("BinaryTournament2",
+					parameters);
+
+			
+			// Add the operators to the algorithm
+			algorithm.addOperator("crossover", crossover);
+			algorithm.addOperator("mutation", mutation);
+			algorithm.addOperator("selection", selection);
+			
+
+			// Add the indicator object to the algorithm
+			algorithm.setInputParameter("indicators", indicators);
+			
+			
+			
+			// Execute the Algorithm
+			long initTime = System.currentTimeMillis();
+			SolutionSet population = algorithm.execute();
+			long estimatedTime = System.currentTimeMillis() - initTime;
+
+			// Result messages
+			logger_.info("Total execution time: " + estimatedTime + "ms");
+			logger_.info("Variables values have been writen to file VAR");
+			population.printVariablesToFile("InitializationResults\\NSGAIIWithoutTrack\\VAR_seed_"+seed[i]);
+			logger_.info("Objectives values have been writen to file FUN");
+			population.printObjectivesToFile("InitializationResults\\NSGAIIWithoutTrack\\FUN_seed_"+seed[i]);
+		}
+	
+	}
+}
+
+class SPEA2_Run{
+	public static Logger logger_; // Logger object
+	public static FileHandler fileHandler_; // FileHandler object
+
+	
+	Problem problem; // The problem to solve
+	Algorithm algorithm; // The algorithm to use
+	Operator crossover; // Crossover operator
+	Operator mutation; // Mutation operator
+	Operator selection; // Selection operator
+
+	HashMap parameters; // Operator parameters
+
+	QualityIndicator indicators; // Object to get quality indicators
+	
+	public void run() throws SecurityException, IOException, ClassNotFoundException, JMException{
+		logger_ = Configuration.logger_;
+		fileHandler_ = new FileHandler("NSGAII_main.log");
+		logger_.addHandler(fileHandler_);
+
+		//seed for spea2
+		long seed [] = {154568, 148456, 447514, 458475, 274587, 712584, 975572, 585464, 467542, 686544 };
+		
+		int numberOfRun=10;
+		for (int i = 0; i < numberOfRun; i++) {
+			
+			
+			PseudoRandom.setRandomGenerator(new RandomGenerator(seed[i]));
+			
+			indicators = null;
+
+			problem = new EnergyPLANProblemAalborg2ObjectivesWith1EnergyPLANEvolution("Real");
+			//algorithm = new NSGAIITrackIndicators(problem, seed[i], "SBX_DKMutation");
 			
 			parameters = new HashMap();
 						
@@ -84,7 +164,7 @@ public class AalborgProblemWithNormalInitilizationMain {
 			
 			// Algorithm parameters
 			algorithm.setInputParameter("populationSize", 100);
-			algorithm.setInputParameter("maxEvaluations", 7000);
+			algorithm.setInputParameter("maxEvaluations", 5000);
 			 algorithm.setInputParameter("archiveSize",100);
 			
 			// Mutation and Crossover for Real codification
@@ -131,6 +211,21 @@ public class AalborgProblemWithNormalInitilizationMain {
 			population.printVariablesToFile("InitializationResults\\SPEA2WithoutTrack\\VAR_seed_"+seed[i]);
 			logger_.info("Objectives values have been writen to file FUN");
 			population.printObjectivesToFile("InitializationResults\\SPEA2WithoutTrack\\FUN_seed_"+seed[i]);
+		}
+	
+	}
+}
+
+
+public class AalborgProblemWithNormalInitilizationMain {
+
+	public static void main(String args[]) throws SecurityException, ClassNotFoundException, IOException, JMException{
+		if(args[0].equals("NSGAII")){
+			NSGAII_Run nsgaii = new NSGAII_Run();
+			nsgaii.run();
+		}else if(args[0].equals("SPEA2")){
+			SPEA2_Run spea2 = new SPEA2_Run();
+			spea2.run();
 		}
 	}
 }
