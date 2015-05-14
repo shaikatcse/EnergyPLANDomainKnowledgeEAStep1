5e14be6a700a719145415251a1b20b44e2308e14
shaikatcse <shaikatcse@gmail.com>
2015-05-14 13:45:40 +0200

    Latex foder is added to have latex table for mean, StD, median and IRQ. R script id added to have combined results of SPEA2 and NSGAII

---
+++ InitializationResults/combined.NSGAIIandSPEA2.R
@@ -0,0 +1,97 @@
+#this scipt is used to generate a boxplot with four indicator (hypervolume, IGD, Epsilon, spead)
+
+#postscript("indicators_NSGAII_SPEA2.eps", horizontal=FALSE, onefile=FALSE, height=8, width=12, pointsize=10)
+#pdf("combined_comparison.pdf", onefile=FALSE, width=8.5, height = 6)
+jpeg(filename = "combined_comparison.jpeg", width = 8.5, height = 6, units = "in", pointsize = 10, res = 1000)
+
+
+NSGAIIresultDirectory<-"."
+
+NSGAIIqIndicator <- function(indicator)
+{
+	fileNSGAII<-paste(NSGAIIresultDirectory, "NSGAIIWithoutTrack", sep="/")
+#filePolynomialMutation<-paste(filePolynomialMutation, problem, sep="/")
+	fileNSGAII<-paste(fileNSGAII, indicator, sep="/")
+	NSGAII_results<-scan(fileNSGAII)
+
+fileNSGAIIsi<-paste(NSGAIIresultDirectory, "NSGAIIWithoutTrackWithSI", sep="/")
+#fileDKMutation<-paste(fileDKMutation, problem, sep="/")
+fileNSGAIIsi<-paste(fileNSGAIIsi, indicator, sep="/")
+NSGAIIsi_results<-scan(fileNSGAIIsi)
+
+algs<-c("RI","SI")
+boxplot(NSGAII_results,NSGAIIsi_results,names=algs, notch = FALSE)
+titulo <-paste(indicator, "NSGAII", sep=":")
+title(main=titulo)
+}
+
+
+SPEA2resultDirectory<-"."
+
+SPEA2qIndicator <- function(indicator)
+{
+	fileSPEA2<-paste(SPEA2resultDirectory, "SPEA2WithoutTrack", sep="/")
+#filePolynomialMutation<-paste(filePolynomialMutation, problem, sep="/")
+	fileSPEA2<-paste(fileSPEA2, indicator, sep="/")
+	SPEA2_results<-scan(fileSPEA2)
+
+fileSPEA2si<-paste(SPEA2resultDirectory, "SPEA2WithoutTrackWithSI", sep="/")
+#fileDKMutation<-paste(fileDKMutation, problem, sep="/")
+fileSPEA2si<-paste(fileSPEA2si, indicator, sep="/")
+SPEA2si_results<-scan(fileSPEA2si)
+
+algs<-c("RI","SI")
+boxplot(SPEA2_results,SPEA2si_results,names=algs, notch = FALSE)
+titulo <-paste(indicator, "SPEA2", sep=":")
+title(main=titulo)
+}
+
+#SPEA2resultDirectory<-"./MutationStudySPEA2/data/"
+
+#SPEA2qIndicator <- function(indicator, problem)
+#{
+#filePolynomialMutation<-paste(SPEA2resultDirectory, "PolynomialMutation", sep="/")
+#filePolynomialMutation<-paste(filePolynomialMutation, problem, sep="/")
+#filePolynomialMutation<-paste(filePolynomialMutation, indicator, sep="/")
+#PolynomialMutation<-scan(filePolynomialMutation)
+
+#fileDKMutation<-paste(SPEA2resultDirectory, "DKMutation", sep="/")
+#fileDKMutation<-paste(fileDKMutation, problem, sep="/")
+#fileDKMutation<-paste(fileDKMutation, indicator, sep="/")
+#DKMutation<-scan(fileDKMutation)
+
+#algs<-c("Polynomial","DKMutation")
+#boxplot(PolynomialMutation,DKMutation,names=algs, notch = FALSE)
+#titulo <-paste(indicator, "SPEA2", sep=":")
+#title(main=titulo)
+#}
+
+par(mfrow=c(2,4))
+indicator<-"HV"
+NSGAIIqIndicator(indicator)
+indicator<-"IGD"
+NSGAIIqIndicator(indicator)
+indicator<-"Epsilon"
+NSGAIIqIndicator(indicator)
+indicator<-"Spread"
+NSGAIIqIndicator(indicator)
+
+indicator<-"HV"
+SPEA2qIndicator(indicator)
+indicator<-"IGD"
+SPEA2qIndicator(indicator)
+indicator<-"Epsilon"
+SPEA2qIndicator(indicator)
+indicator<-"Spread"
+SPEA2qIndicator(indicator)
+
+#indicator<-"HV"
+#SPEA2qIndicator(indicator, "OptimizeElecEnergy_SPEA2")
+#indicator<-"IGD"
+#SPEA2qIndicator(indicator, "OptimizeElecEnergy_SPEA2")
+#indicator<-"GD"
+#SPEA2qIndicator(indicator, "OptimizeElecEnergy_SPEA2")
+#indicator<-"Epsilon"
+#SPEA2qIndicator(indicator, "OptimizeElecEnergy_SPEA2")
+
+dev.off()
